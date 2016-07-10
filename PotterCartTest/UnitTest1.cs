79e9ece22e5f8aa878becfa229f4c68aba63a4d5
Asus <Asus@DESKTOP-9QJD8V2>
2016-07-10 16:11:22 +0800

    Add project files.

---
+++ PotterCartTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterCartTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
