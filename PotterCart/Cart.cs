d8233b3c9fad69fa8d1f3b025dd42bd844b74ddc
Jamie <preazzi@gmail.com>
2016-07-10 17:19:42 +0800

    加入第一個試案例 - 第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ PotterCart/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PotterCart
+{
+    public class Cart
+    {
+        public Cart()
+        {
+        }
+
+        public void Add(Volume first)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterCartTest/CartTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterCart;
+
+namespace PotterCartTest
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void BuyFirstPriceShouldBe100()
+        {
+            //arrange
+            var cart = new Cart();
+
+            //act
+            cart.Add(Volume.First);
+
+            //assert
+            int expected = 100;
+            Assert.AreEqual(expected, cart.GetPrice());
+        }
+    }
+}
