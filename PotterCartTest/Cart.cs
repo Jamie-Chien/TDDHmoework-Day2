e553ba14e0114a7fd7ddcb5c7363f696d52a7b20
Jamie <preazzi@gmail.com>
2016-07-13 23:26:30 +0800

    通過第五個測試案例

--- PotterCartTest/Cart.cs
+++ PotterCartTest/Cart.cs
@@ -7,7 +7,7 @@ namespace PotterCartTest
     class Cart
     {
         private List<Book> Books;
-        private readonly double[] discountTable = new double[] { 1, 0.95, 0.9, 0.8 };
+        private readonly double[] discountTable = new double[] { 1, 0.95, 0.9, 0.8, 0.75 };
 
         public Cart()
         {
