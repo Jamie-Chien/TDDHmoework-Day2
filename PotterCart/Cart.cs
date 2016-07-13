5aa3dc60bbab457efae12dbd0b5a46110cb5def6
Jamie <preazzi@gmail.com>
2016-07-13 22:55:14 +0800

    重構 Production Code

--- PotterCart/Cart.cs
+++ PotterCart/Cart.cs
@@ -22,27 +22,31 @@ namespace PotterCart
 
         public int GetPrice()
         {
-            int price = 0;
-            var tempVolumes = new List<Volume>();
+            return CalcualtePrice(Volumes, 0);
+        }
 
-            while (Volumes.Count != 0)
+        private int CalcualtePrice(List<Volume> volumes, int price)
+        {
+            if (volumes.Count > 0)
             {
-                var purchasedVolume = Volumes.Distinct().ToList();
+                var purchasedVolume = volumes.Distinct().ToList();
+                removeElementFromList(ref volumes, purchasedVolume);
 
                 int purchasedVolumeCount = purchasedVolume.Count();
                 double discount = discountRange[purchasedVolumeCount - 1];
-                price += (int)Math.Round(purchasedVolumeCount * 100 * discount);
 
-                foreach (var item in purchasedVolume)
-                {
-                    tempVolumes.Add(item);
-                    Volumes.Remove(item);
-                }
+                int priceOfGroup = (int)Math.Round(purchasedVolumeCount * 100 * discount);
+                return priceOfGroup + CalcualtePrice(volumes, price);
             }
-
-            Volumes = tempVolumes;
-
             return price;
+        }
+
+        private void removeElementFromList(ref List<Volume> volumes, List<Volume> removeList)
+        {
+            foreach (var item in removeList)
+            {
+                volumes.Remove(item);
+            }
         }
     }
 }
